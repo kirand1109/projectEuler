12f46a2d4dd21649a9ad1209e7c15dfd77919ce7
Kiran Kumar Donthula <32912966+kirand1109@users.noreply.github.com>
2019-03-09 17:50:00 -0600

    Update self-powers.py

--- self-powers.py
+++ self-powers.py
@@ -1,6 +1,4 @@
-'''The series, 11 + 22 + 33 + ... + 1010 = 10405071317.
-
-Find the last ten digits of the series, 11 + 22 + 33 + ... + 10001000.'''
+'''The series, 11 + 22 + 33 + ... + 1010 = 10405071317.Find the last ten digits of the series, 11 + 22 + 33 + ... + 10001000.'''
 
 import sys, os, math
 
@@ -10,8 +8,10 @@ for i in range (1, 1001):
     x=i**i
     sum=sum+x
 
-#print sum
+# converting integer to string
 num=str(sum)
+
+#printing last 10 digits
 print num[-10:]
 
 
