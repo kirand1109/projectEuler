6c7740f7ca461c517700790a7ecf901a0d9bda6e
kirand1109 <kirand1109@gmail.com>
2019-09-02 17:37:38 -0500

    first commit

--- mean.py
+++ mean.py
@@ -4,7 +4,10 @@
 
 student_grades = [9.1, 8.8, 7.5, 9.7, 8.6]
 
+# sum is inbuilt command
 mysum = sum(student_grades)
+
+# finding the number of elements present in the list
 length = len(student_grades)
 
 mean = mysum/length
